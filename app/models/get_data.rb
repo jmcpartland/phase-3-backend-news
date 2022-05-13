6066a5bd619ffafe1c70a3c59d394c648e60524a
Joe McPartland <joemcpartland@gmail.com>
2022-05-13 13:56:26 -0600

    seed file progress 2

--- app/models/get_data.rb
+++ app/models/get_data.rb
@@ -13,13 +13,23 @@ class GetData < ActiveRecord::Base
     def self.source_seed
         self.grab
         news_seeding = @news['articles'].map {|x| "Source.create(name: '#{x['source']['name']}'),"}.uniq.join("\n")
-        seed_file = File.open('seed_file.rb', 'w') { |f| f.write news_seeding }
+        seed_file = File.open('seed_file.rb', 'a') { |f| f.write news_seeding }
     end
+
+
+    def escape_char (field)
+        field.map { |f| f === "'" ? f = "\'" : false}
+    end
+
+# Figure out a way to escape character's such as ' in the titles fields
 
     def self.article_seed
         self.grab
-        news_seeding = @news['articles'].map {|x| "Article.create(name: '#{x['author']}'),"}.join("\n")
-        seed_file = File.open('seed_file.rb', 'a') { |f| f.write news_seeding }
+        news_seeding = @news['articles'].map { |x|
+            "Article.create(author: '#{x['author']}', title: '#{x['title']}', description: '#{x['description']}'),"
+        }.join("\n")
+            seed_file = File.open('seed_file.rb', 'a') { |f| f.write news_seeding }
+        
     end
 
 end
