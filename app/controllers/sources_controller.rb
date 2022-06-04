37b2c5cda2b455fd633340c6f547eec3c19b7477
Joe McPartland <joemcpartland@gmail.com>
2022-06-04 16:20:04 -0600

    created REST Show routes

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,8 +1,18 @@
 class SourcesController < ApplicationController
 
+    # Souces show route
     get '/sources' do
         sources = Source.all
-        sources.to_json #(include: :articles)
+        sources.to_json(include: :articles)
     end
 
+    # Souces show route
+    get '/sources/:id' do
+        source = Source.find_by(id: params[:id])
+        if source
+            source.to_json(include: :articles)
+        else
+            "404 - Source not found"
+        end
+    end
 end
