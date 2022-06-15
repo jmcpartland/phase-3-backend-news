c8e0f5bbd61c25560135c4c61a3108f915dc3af5
Joe McPartland <joemcpartland@gmail.com>
2022-06-15 15:06:22 -0600

    article controller edits

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,13 @@ class ArticlesController < ApplicationController
         Article.all.to_json
     end
 
+    post '/articles' do
+        article = Article.create(params).to_json
+    end
+
+    delete '/articles/:id' do
+        article = Article.find_by(id: params(:id))
+        article.destroy
+    end
+
 end
